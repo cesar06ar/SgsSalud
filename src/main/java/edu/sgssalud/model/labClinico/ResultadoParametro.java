2e12f3e347014f6692b48220531d971aa6ada296
cesar06ar <cesar06ar@gmail.com>
2014-06-20 00:14:49 -0500

    proyecto final..... v6.2  2014-06-15 Completo

--- src/main/java/edu/sgssalud/model/labClinico/ResultadoParametro.java
+++ src/main/java/edu/sgssalud/model/labClinico/ResultadoParametro.java
@@ -17,6 +17,7 @@ package edu.sgssalud.model.labClinico;
 
 import java.io.Serializable;
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -36,6 +37,7 @@ public class ResultadoParametro implements Serializable, Comparable<ResultadoPar
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String nombre;
+    @Column(length = 1500)
     private String valor;
     private String unidadMedida;
     private String categoria;
